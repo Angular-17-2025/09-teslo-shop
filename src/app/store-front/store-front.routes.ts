2202f505d00bf4f9daf206b7eafa87d1f7891315
AdrianRDG <adrian.rojas@tergar.org>
2025-07-31 08:50:21 -0600

    Feat: add pages and routes with lazzy load for store-front path

--- src/app/store-front/store-front.routes.ts
+++ src/app/store-front/store-front.routes.ts
@@ -5,7 +5,22 @@ export const storeFrontRoutes: Routes = [
     path: '',
     loadComponent: () => import('./layouts/store-front-layout/store-front-layout').then(m => m.StoreFrontLayout),
     children: [
-
+      {
+        path: '',
+        loadComponent: () => import('./pages/home-page/home-page').then(m => m.HomePage)
+      },
+      {
+        path: 'gender/:gender',
+        loadComponent: () => import('./pages/gender-page/gender-page').then(m => m.GenderPage)
+      },
+      {
+        path: 'product/:product_id',
+        loadComponent: () => import('./pages/product-page/product-page').then(m => m.ProductPage)
+      },
+      {
+        path: '**',
+        loadComponent: () => import('./pages/not-found-page/not-found-page').then(m => m.NotFoundPage)
+      }
     ],
   },
   {
