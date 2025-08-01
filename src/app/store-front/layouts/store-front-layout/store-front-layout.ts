520177bfd1a627006e25a65e6869c2ae467d0ad7
AdrianRDG <adrian.rojas@tergar.org>
2025-07-31 18:41:45 -0600

    Update: add router-outlet to load children routes

--- src/app/store-front/layouts/store-front-layout/store-front-layout.ts
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FrontNavbar } from "../../components/front-navbar/front-navbar";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [FrontNavbar],
+  imports: [FrontNavbar, RouterOutlet],
   templateUrl: './store-front-layout.html',
   styles: ``
 })
