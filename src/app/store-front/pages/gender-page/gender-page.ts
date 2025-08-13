e1a3bd17085bcfb8108e859a54e4fbf8e552f938
AdrianRDG <adrian.rojas@tergar.org>
2025-08-12 21:43:42 -0600

    Feat: get products by gender with rxResource | reactive way

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCard } from "@products/components/product-card/product-card";
+import { ProductsService } from '@products/services/products-service';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard, TitleCasePipe],
   templateUrl: './gender-page.html',
   styles: ``
 })
 export class GenderPage {
+
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+  gender = signal<string>('');
+
+  productGenderResource = rxResource({
+    stream: () => this.route.paramMap.pipe(
+      map((params) => this.gender.set(params.get('gender')!)),
+      switchMap(() => this.productsService.getProducts({ gender: this.gender() }))
+    )
+  });
 
 }
