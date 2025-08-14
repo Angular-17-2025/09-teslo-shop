63cb75c2728636d18f9cc58f54c92d2d0e5fd11d
AdrianRDG <adrian.rojas@tergar.org>
2025-08-13 20:10:44 -0600

    Update: add page to queryParam in the url and create activePage to change button in navigation

--- src/app/shared/pagination/pagination.ts
+++ src/app/shared/pagination/pagination.ts
@@ -1,8 +1,9 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.html',
   styles: ``
 })
@@ -10,6 +11,7 @@ export class Pagination {
 
   totalPages = input<number>(0);
   currentPage = input<number>(1);
+  activePage = linkedSignal(this.currentPage);
 
   gatePageList = computed(() => {
     return Array.from( { length: this.totalPages() }, (_, index) => index + 1 );
