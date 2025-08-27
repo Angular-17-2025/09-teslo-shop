6088c43d8a142a5cc85db8a5db108f0eaf5f5a17
AdrianRDG <adrian.rojas@tergar.org>
2025-08-26 18:41:49 -0600

    Feat: add auth.routes with its components and lazy load

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from "@angular/router";
+
+export const AuthRoutes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () => import('./layout/auth-layout/auth-layout').then(m => m.AuthLayout),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login-page/login-page').then(m => m.LoginPage)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/register-page/register-page').then(m => m.RegisterPage)
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+
+];
+
+export default AuthRoutes;
