421cfcf575515ae3ebd9eb290efd365e2c8fce6a
AdrianRDG <adrian.rojas@tergar.org>
2025-09-25 13:41:20 -0600

    Update: add checkLoginstatusn function to check if user is logged in

--- src/app/auth/services/auth-services.ts
+++ src/app/auth/services/auth-services.ts
@@ -1,8 +1,10 @@
+import { LoginResponseInterface } from './../interfaces/login-response.interface';
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { LoginResponseInterface } from '@auth/interfaces/login-response.interface';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { UserInterface } from '@auth/interfaces/user.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 
@@ -17,6 +19,12 @@ export class AuthServices {
 
   private _http = inject(HttpClient);
 
+  private API_BASE_URL = environment.API_BASE_URL;
+
+  checkStatus = rxResource({
+    stream: () => this.checkLoginstatus()
+  });
+
   authStatus = computed<AuthStatus>(() => {
     if(this._authStatus() === 'checking') return 'checking';
 
@@ -30,6 +38,38 @@ export class AuthServices {
 
   login(email: string, password: string): Observable<boolean>{
     return this._http.post<LoginResponseInterface>('http://localhost:3000/api/auth/login', { email, password }).pipe(
+      tap(resp => {
+        this._user.set(resp.user);
+        this._authStatus.set('authenticated');
+        this._token.set(resp.token);
+
+        localStorage.setItem('token', resp.token);
+      }),
+      map(() => true),
+      catchError((error) => {
+        console.log(error);
+        this._user.set(null);
+        this._token.set(null);
+        this._authStatus.set('not-authenticated');
+        localStorage.removeItem('token');
+        return of(error);
+      })
+    );
+  }
+
+  checkLoginstatus(): Observable<boolean>{
+
+    let token = localStorage.getItem('token');
+
+    if(!token) {
+      return of(false);
+    }
+
+    return this._http.get<LoginResponseInterface>(`${this.API_BASE_URL}/auth/check-status`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).pipe(
       tap(resp => {
         this._user.set(resp.user);
         this._authStatus.set('authenticated');
