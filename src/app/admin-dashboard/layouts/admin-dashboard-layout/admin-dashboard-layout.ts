46f874648cc1cfe62aa5edb1500a55f8b54f0fb0
AdrianRDG <adrian.rojas@tergar.org>
2025-10-08 18:39:21 -0600

    Update: add layout for admin-dashboard-layout component

--- src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.ts
+++ src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthServices } from '@auth/services/auth-services';
 
 @Component({
   selector: 'app-admin-dashboard-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard-layout.html',
   styles: ``
 })
 export class AdminDashboardLayout {
+
+  authService = inject(AuthServices)
+
+  user = computed(() => this.authService.user());
 
 }
