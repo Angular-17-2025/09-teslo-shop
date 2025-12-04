53f489a23e292a698942a470b0f56d3f1633cad1
AdrianRDG <adrian.rojas@tergar.org>
2025-12-04 13:34:12 -0600

    Update: add changeProductsPerPage effect to reset page when user change productsPerPage input

--- src/app/admin-dashboard/pages/admin-products/admin-products.ts
+++ src/app/admin-dashboard/pages/admin-products/admin-products.ts
@@ -7,6 +7,7 @@ import { map } from 'rxjs';
 import { PaginationService } from 'src/app/shared/pagination/pagination-service';
 import { Pagination } from "src/app/shared/pagination/pagination";
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-products',
@@ -20,6 +21,31 @@ export class AdminProducts {
   readonly paginationService = inject(PaginationService);
   productsPerPage = signal<number>(10);
   products = signal<ProductResponseInterface | null>(null);
+  router = inject(Router);
+
+  changeProductsPerPage = effect(() => {
+    const newLimit = this.productsPerPage();
+    const currentPage = this.paginationService.pageNumberFromURL();
+
+    // Get the total number of items from the resource response
+    const totalItems = this.products()?.count ?? 0;
+
+    // Calculate the maximum possible page number for the new limit(.ceil() round to entire number)
+    const maxPage = totalItems > 0 ? Math.ceil(totalItems / newLimit) : 1;
+
+    // Check if the current page is greater than the new max page
+    // or if the current page is 0 (shouldn't happen, but good safeguard)
+    if (currentPage > maxPage || currentPage < 1) {
+      // Navigate to the same route, but set the page query param to 1
+      this.router.navigate(
+        [],
+        {
+          queryParams: { page: 1 },
+          queryParamsHandling: 'merge' // Keep other query params if there are any
+        }
+      );
+    }
+  }, { allowSignalWrites: true });
 
   productsResource = rxResource({
     params: () => ({
@@ -27,7 +53,7 @@ export class AdminProducts {
       limit: this.productsPerPage()
     }),
     stream: ({params}) => this.productsService.getProducts({
-      offset: params.page * 9,
+      offset: params.page * params.limit,
       limit: params.limit
     }).pipe(
       map((resp) => this.products.set(resp))
