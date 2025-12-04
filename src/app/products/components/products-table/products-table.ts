b4e26534b2c20cc0735a87f7d4d7a375fe05a63b
AdrianRDG <adrian.rojas@tergar.org>
2025-12-03 18:28:59 -0600

    Feat: add products-table component with input to get products

---
+++ src/app/products/components/products-table/products-table.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { Product } from '@products/interfaces/product-response-interface';
+import { ProductImagePipe } from '@products/pipes/product-image-pipe';
+import { RouterLink } from "@angular/router";
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-products-table',
+  imports: [ProductImagePipe, RouterLink, CurrencyPipe, AsyncPipe],
+  templateUrl: './products-table.html',
+  styles: ``
+})
+export class ProductsTable {
+
+  products = input.required<Product[]>();
+
+}
