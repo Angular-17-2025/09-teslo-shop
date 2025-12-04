2f47da0fa47fa3a74401c3ad2bed0d7f4f26c4a7
AdrianRDG <adrian.rojas@tergar.org>
2025-12-03 18:30:09 -0600

    Update: add products-table component to /admin/products page

--- src/app/admin-dashboard/pages/admin-products/admin-products.ts
+++ src/app/admin-dashboard/pages/admin-products/admin-products.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsTable } from "@products/components/products-table/products-table";
+import { ProductResponseInterface } from '@products/interfaces/product-response-interface';
+import { ProductsService } from '@products/services/products-service';
+import { map } from 'rxjs';
+import { PaginationService } from 'src/app/shared/pagination/pagination-service';
+import { Pagination } from "src/app/shared/pagination/pagination";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-products',
-  imports: [],
+  imports: [ProductsTable, Pagination, FormsModule],
   templateUrl: './admin-products.html',
   styles: ``
 })
 export class AdminProducts {
+
+  private productsService = inject(ProductsService);
+  readonly paginationService = inject(PaginationService);
+
+  products = signal<ProductResponseInterface | null>(null);
+
+  productsResource = rxResource({
+    params: () => ({page: this.paginationService.pageNumberFromURL() - 1}),
+    stream: ({params}) => this.productsService.getProducts({offset: params.page * 9}).pipe(
+      map((resp) => this.products.set(resp))
+    )
+  });
 
 }
