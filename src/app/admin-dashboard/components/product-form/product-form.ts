ccf07fbea3b6ba51ed73051690b0f668ffe49c24
AdrianRDG18 <kersdeg18@gmail.com>
2025-12-09 15:11:11 -0600

    Update: add productForm & include slugPattern as pattern validator

--- src/app/admin-dashboard/components/product-form/product-form.ts
+++ src/app/admin-dashboard/components/product-form/product-form.ts
@@ -1,17 +1,45 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Product } from '@products/interfaces/product-response-interface';
 import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/slug-pattern';
 
 @Component({
   selector: 'app-product-form',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-form.html',
   styles: ``
 })
-export class Productform {
+export class Productform  implements OnInit{
 
   product = input.required<Product>();
+  formBuilder = inject(FormBuilder);
 
-  sizes = ['XS', 'SM', 'MD', 'L', 'XL', 'XLL']
+  sizes = ['XS', 'SM', 'MD', 'L', 'XL', 'XLL'];
+
+  productForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    description: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+    sizes: [['']],
+    images: [[]]
+  });
+
+  ngOnInit(): void {
+    this.setFormValues(this.product());
+  }
+
+  setFormValues(product: Partial<Product>) {
+    this.productForm.reset(product as any);
+    this.productForm.patchValue({tags: product.tags?.join(', ')});
+  }
+
+  submitForm() {
+    console.log(this.productForm.value);
+  }
 
 }
